9e106af847cbb9f6bf80e3233dd30f035fe66b53
Romena Murgor <romenamurgor@gmail.com>
2020-03-10 03:59:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [array] << "1","2","3","4"
-  puts array.inspect 
+  
 end
 
 def add_element_to_start_of_array(array, element)
